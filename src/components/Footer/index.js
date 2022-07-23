c16068b0f74d155bb125e48568e30368b8da60a4
Erik Dell <edellenator@gmail.com>
2022-07-23 10:20:24 -0700

    footer bug fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="sticky-bottom">
             <div className="container">
                 <div className="row">
                     <div className="column">
