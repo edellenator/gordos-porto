e393f485b32b2100efc97314cb6a97aeea5f68f3
Erik Dell <edellenator@gmail.com>
2022-07-23 10:18:12 -0700

    portfolio page layout

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,7 +3,7 @@ import React from "react";
 const Portfolio = () => {
     return (
         <div className="container justify-content-between">
-            <div className="row justify-content-center">
+            <div className="row justify-content-between">
 
                 <div className="card text-bg-white col-3 m-2 border border-0">
                     <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
@@ -11,36 +11,37 @@ const Portfolio = () => {
                         <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
-                <div className="card text-bg-light col-3 m-2">
-                    <img className="card-img m-0" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
+                <div className="card text-bg-white col-3 m-2 border border-0">
+                    <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
-                <div className="card text-bg-light col-3 m-2">
-                    <img className="card-img m-0" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
+                <div className="card text-bg-white col-3 m-2 border border-0">
+                    <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
-                <div className="card text-bg-light col-3 m-2">
-                    <img className="card-img m-0" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
+                <div className="card text-bg-white col-3 m-2 border border-0">
+                    <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
-                <div className="card text-bg-light col-3 m-2">
-                    <img className="card-img m-0" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
+                <div className="card text-bg-white col-3 m-2 border border-0">
+                    <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
-                <div className="card text-bg-light col-3 m-2">
-                    <img className="card-img m-0" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
+                <div className="card text-bg-white col-3 m-2 border border-0">
+                    <img className="card-img" src={require('../assets/images/placeholderimg.png')} alt="placeholder-image" />
                     <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title align-middle">Card title</h5>
                     </div>
                 </div>
+                
                 
             </div>
         </div>
