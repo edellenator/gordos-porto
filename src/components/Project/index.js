914e132c2c3f03a815d3b36adff7460feb3a1779
Erik Dell <edellenator@gmail.com>
2022-11-01 22:03:53 -0700

    created simple carousel for portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,34 +7,50 @@ const Project = (props) => {
     const {projects = []} = props
     console.log(projects)
     return (
-        <div className="container">
-            <div className="row justify-content-between">
-                {projects.map((project) => (
-                <div className="card text-bg-white col-3 m-1 border border-0">
-                    <img 
-                        src={require(`../../assets/images/${project.id}.png`)} 
-                        alt={project.name}
-                        className="card-img flex-fill project-img" 
-                        key={project.name}
-                    />
-                    <div className="card-img-overlay">
-                        <div className="project-button">
-                            <h3 className="card-title">{project.name}</h3>
-                            <p className="card-text">{project.description}</p>
-                            <p>
-                                <a href={project.github} target="_blank">
-                                    <FontAwesomeIcon icon={faGithub} size="2x" />
-                                </a>
-                                <span className="m-4">
-                                <a href={project.deployed} target="_blank">
-                                    <FontAwesomeIcon icon={faLink} size="2x" />
-                                </a>
-                                </span>
-                            </p>
-                        </div>
+        <div className="row">
+
+            <div className="slider col-12">
+                {/* <div className="row justify-content-between"> */}
+                    {projects.map((project) => (
+                        <a href={`#slide-${project.id}`}>{project.id}</a>
+                    // <div className="card text-bg-white col-3 m-1 border border-0">
+                    //     <img 
+                    //         src={require(`../../assets/images/${project.id}.png`)} 
+                    //         alt={project.name}
+                    //         className="card-img project-img" 
+                    //         key={project.name}
+                    //     />
+                    //     <div className="card-img-overlay">
+                    //         <div className="project-button">
+                    //             <h3 className="card-title">{project.name}</h3>
+                    //             <p className="card-text">{project.description}</p>
+                    //             <p>
+                    //                 <a href={project.github} target="_blank">
+                    //                     <FontAwesomeIcon icon={faGithub} size="2x" />
+                    //                 </a>
+                    //                 <span className="m-4">
+                    //                 <a href={project.deployed} target="_blank">
+                    //                     <FontAwesomeIcon icon={faLink} size="2x" />
+                    //                 </a>
+                    //                 </span>
+                    //             </p>
+                    //         </div>
+                    //     </div>
+                    // </div>
+                    ))}
+                    <div className="slides">
+                        {projects.map((project) => (
+                            <div id={`slide-${project.id}`}>
+                                <img 
+                                    src={require(`../../assets/images/${project.id}.png`)} 
+                                    alt={project.name}
+                                    className="card-img project-img" 
+                                    key={project.name}
+                                />
+                            </div>
+                        ))}
                     </div>
-                </div>
-                ))}   
+                {/* </div> */}
             </div>
         </div>
     );
